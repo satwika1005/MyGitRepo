f544fdd9e0e084cf1fa0d4a4115f4d6fa0e9e528
Satwika05 <skondi@gmu.edu>
2024-12-04 15:18:06 -0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeList
+        public ActionResult GetEmployeeList()
+        {
+            EmployeeEntities employeeEntities = new EmployeeEntities();
+            List<Employee> employees = employeeEntities.Employees.ToList();
+            return View(employees);
+        }
+
+        // GetEmployeeById
+        public ActionResult GetById(int id)
+        {
+            EmployeeEntities employeeentities = new EmployeeEntities();
+            Employee employee = employeeentities.Employees.Find(id);
+            return View(employee);
+        }
+    }
+}
